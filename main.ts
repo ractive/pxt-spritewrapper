9f16cc3033256bcd9bedae4d054a6e97034afd52
Jean-Pierre Bergamin <james.bergamin@local.ch>
2020-05-09 12:14:02 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,60 @@
+ namespace SpriteWrapper {
+    const objects: { [id: string]: SpriteWrapper } = {};
+
+    /**
+     * Classes that wrap a Sprite and want to register instances
+     * of it implement this interface. The onDestroyed method is called
+     * when the corresponding sprite is destroyed.
+     */
+    export interface SpriteWrapper {
+        sprite: Sprite;
+        onDestroyed(): void;
+    }
+
+	
+    /**
+     * An abstract base class you can use to derive your classes from.
+     * All instances are registered with the "register" function after
+     * being created.
+     */
+    export abstract class Support implements SpriteWrapper {
+        public readonly sprite: Sprite;
+        constructor(sprite: Sprite) {
+            this.sprite = sprite;
+            register(this);
+        }
+
+        public onDestroyed(): void { };
+    }
+
+    /**
+     * Registers a SpriteWrapper object whose onDestroyed
+     * mehtod is called, when the wrapped sprite is destroyed.
+     */
+    export function register(object: SpriteWrapper): void {
+        objects[object.sprite.id] = object;
+
+        object.sprite.onDestroyed(() => {
+            object.onDestroyed();
+            delete objects[object.sprite.id];
+        });
+    }
+
+    /**
+     * Return the object that is associated with the given sprite.
+     * This can be useful when a sprite is retrieved e.g. with
+     * sprites.onOverlap(...) and you want to have the
+     * corresponding SpriteWrapper object.
+     * 
+     */
+    export function fromSprite(sprite: Sprite): SpriteWrapper {
+        return objects[sprite.id];
+    }
+
+    /**
+     * Returns all registered SpriteWrapper objects
+     */
+    export function all(): SpriteWrapper[] {
+        return objects.values();
+    }
+}
